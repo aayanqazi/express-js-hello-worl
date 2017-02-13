fc900a137d0990f55d360926c716791239762ae6
aayanqazi <aayanqazi@yahoo.com>
2017-02-13 20:22:57 +0500

    Complete Express Training

--- server.js
+++ server.js
@@ -1,6 +1,62 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.json())
+
+var studentSchema = new mongoose.Schema({
+    name: String,
+    rollNumber: Number,
+    class: Number
+})
+
+var studentModel = mongoose.model("students", studentSchema);
+
+app.post("/add", function (req, res, next) {
+    var newStudent = new studentModel({
+        name: req.body.names,
+        rollNumber: req.body.rollNumber,
+        class: req.body.class
+    })
+
+    newStudent.save(function (err, data) {
+        if (!err) {
+            console.log("Student is Saved !!")
+        }
+        else {
+            console.log("Failedd !!!");
+            res.send("Failed !!!")
+        }
+    })
+})
+
+app.get("/sub", function (req, res, next) {
+    studentModel.find({}, function (err, data) {
+        if (!err) {
+            console.log(data)
+            res.send(data)
+        }
+        else {
+            console.log("Eror")
+        }
+    })
+
+})
+app.get("/del", function (req, res, next) {
+
+    studentModel.find({name:"Arsalan Sabir"}).remove(function(err,data)
+    {
+        if(!err)
+        {
+            console.log(data)
+        }
+        else
+        {
+            console.log("ERROR")
+        }
+    })
+})
 
 var port = (process.env.PORT || 3000)
 mongoose.connect('mongodb://aayanqazi:xp9999955555@ds151059.mlab.com:51059/hello-world')
@@ -10,12 +66,6 @@ db.once('open', function () {
     console.log("Connected");
 });
 
-app.use('/home', function (req, res) {
-    res.send('Hello World');
-})
-app.use('/news', function (req, res) {
-    res.send('news');
-})
 
 app.listen(port, () => {
     console.log("Server Running At http://localhost:" + port);
