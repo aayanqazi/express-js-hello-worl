367cb498949d0654434e1ed20210a4e7ca98f686
aayanqazi <aayanqazi@yahoo.com>
2017-02-13 16:41:58 +0500

    port change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var port = (process.env.port || 3000)
+var port = (process.env.PORT || 3000)
 app.use('/home',function(req,res)
 {
     res.send('Hello World');
@@ -11,6 +11,6 @@ app.use('/news',function(req,res)
     res.send('news');
 })
 
-app.listen(port);
-console.log("Server Running At http://localhost:3000");
-module.exports = app
+app.listen(port,()=>{
+    console.log("Server Running At http://localhost:"+port);
+});
