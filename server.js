79741351f0b0a83d369f8b24f248433ea8899e89
aayanqazi <aayanqazi@yahoo.com>
2017-02-13 16:17:23 +0500

    change pathe

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
-app.use('/',function(req,res)
+app.use('/home',function(req,res)
 {
     res.send('Hello World');
 })
+app.use('/news',function(req,res)
+{
+    res.send('news');
+})
+
 app.listen(3000);
 console.log("Server Running At http://localhost:3000");
 module.exports = app
