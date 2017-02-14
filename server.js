4e53845aeb2b9492cdd3631c17dfd35adbaf2982
aayanqazi <aayanqazi@yahoo.com>
2017-02-14 12:15:40 +0500

    updated

--- server.js
+++ server.js
@@ -6,30 +6,10 @@ var app = express();
 app.use(bodyParser.json())
 
 var studentSchema = new mongoose.Schema({
-    name: String,
-    rollNumber: Number,
-    class: Number
+    name: String
 })
 
 var studentModel = mongoose.model("students", studentSchema);
-
-app.post("/add", function (req, res, next) {
-    var newStudent = new studentModel({
-        name: req.body.names,
-        rollNumber: req.body.rollNumber,
-        class: req.body.class
-    })
-
-    newStudent.save(function (err, data) {
-        if (!err) {
-            console.log("Student is Saved !!")
-        }
-        else {
-            console.log("Failedd !!!");
-            res.send("Failed !!!")
-        }
-    })
-})
 
 app.get("/sub", function (req, res, next) {
     studentModel.find({}, function (err, data) {
@@ -38,25 +18,12 @@ app.get("/sub", function (req, res, next) {
             res.send(data)
         }
         else {
-            console.log("Eror")
+            console.log("Error")
         }
     })
 
 })
-app.get("/del", function (req, res, next) {
 
-    studentModel.find({name:"Arsalan Sabir"}).remove(function(err,data)
-    {
-        if(!err)
-        {
-            console.log(data)
-        }
-        else
-        {
-            console.log("ERROR")
-        }
-    })
-})
 
 var port = (process.env.PORT || 3000)
 mongoose.connect('mongodb://aayanqazi:xp9999955555@ds151059.mlab.com:51059/hello-world')
