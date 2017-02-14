9f3dc1718c863b5130b32326a03642064f5935c4
aayanqazi <aayanqazi@yahoo.com>
2017-02-14 13:48:32 +0500

    completed Server

--- server.js
+++ server.js
@@ -24,8 +24,23 @@ app.get("/sub", function (req, res, next) {
     })
 
 })
-
-
+app.post("/sub",function(req,res,next)
+{
+    var newAdd = new studentModel({
+        name: req.body.name
+    })
+    newAdd.save(function(err,data)
+    {
+        if(!err)
+        {
+            console.log("Saved");
+        }
+        else
+        {
+            console.log("Error");
+        }
+    });
+})
 var port = (process.env.PORT || 3000)
 mongoose.connect('mongodb://aayanqazi:xp9999955555@ds151059.mlab.com:51059/hello-world')
 var db = mongoose.connection;
